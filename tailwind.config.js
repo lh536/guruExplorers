726aaebebe4fd33879de63e1f7783db2e89774a5
Fabio Duarte <mail.fabio.duarte@gmail.com>
2023-09-12 11:39:17 -0500

    chore: :wrench: Basic tailwind configuration. Add brand colors and reset form input styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        'orange-brand': '#FB7C45',
+        'gray-brand': '#74747C',
+      }
     },
   },
   plugins: [],
