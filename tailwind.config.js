44563621a325cacdddc6b90a080bd0d0439644ff
Fabio Duarte <mail.fabio.duarte@gmail.com>
2023-09-12 23:55:50 -0500

    feat: :lipstick: Add Raleway font. Create footer prototype

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,13 @@ export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        raleway: "'Raleway', sans-serif",
+      },
       colors: {
         'orange-brand': '#FB7C45',
         'gray-brand': '#74747C',
+        'gray-footer': '#343c44',
       }
     },
   },
