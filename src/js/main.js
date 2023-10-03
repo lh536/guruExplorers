37ba55e8e315a2a5f207967d15edb8693b61413e
Fabio Duarte <mail.fabio.duarte@gmail.com>
2023-10-03 09:57:21 -0500

    fix: :bug: Installs 'glidejs@3.6.0' as a dependency. Updates 'gliderjs' .css link tag in 'index.html' and 'tour_detail.html' fixing the issue that doesn't shows the carousel in those pages. Moves script tag from 'index.html' and 'tour_detail.html' to 'main.js'. Adds 'glide.core.min.css.map' and 'glide.theme.min.css.map' to 'src/css' folder, fixing the issue with the non-visible carousel

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,44 @@
-console.log('hello world');
+import Glide from '@glidejs/glide'
+
+
+new Glide (".glide", {
+    autoplay: 5000,
+    hoverpause: true,
+    perView: 4,
+    keyboard: true,
+    gap: 0,
+    type: 'carousel',
+    breakpoints: {
+      768: {
+        perView: 1,
+      },
+      1024 :{
+        perView: 2
+      },
+      1280 :{
+        perView: 3
+      }
+
+    }
+  }).mount();
+
+  new Glide (".glide_tours", {
+    autoplay: 5000,
+    hoverpause: true,
+    perView: 4,
+    keyboard: true,
+    gap: 20,
+    type: 'carousel',
+    breakpoints: {
+      768: {
+        perView: 1,
+      },
+      1024 :{
+        perView: 2
+      },
+      1280 :{
+        perView: 3
+      }
+
+    }
+  }).mount();
